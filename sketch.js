652ca1cb03623c48d9c49c4c5e1400aefa302a56
qdwarf <93176779+qdwarf@users.noreply.github.com>
2021-11-02 15:18:38 +0900

    test 2 objects

--- sketch.js
+++ sketch.js
@@ -3,14 +3,30 @@ let m;
 function setup() {
   createCanvas(400, 400);
   m = new Mover(50, 50, 10);
+  m2 = new Mover (300, 300, 50);
 }
 
 function draw() {
   background(220);
   m.update();
   m.show();
-  
+
+  m2.update();
+  m2.show();
+
   if (mouseIsPressed) {
     //make attract to mouse position
+    let mousePos = createVector(mouseX, mouseY);
+
+    m.attractTo(mousePos);
+    m2.attractTo(mousePos);
+
+    // let f = p5.Vector.sub(mousePos, m.pos);
+    // f.limit(2);
+    // m.applyForce(f);
+    //
+    // let f2 = p5.Vector.sub(mousePos, m2.pos);
+    // f2.limit(2);
+    // m2.applyForce(f2);
   }
 }
